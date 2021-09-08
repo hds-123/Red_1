42132dbd3bf25991cb072e39151d461691a2b8f4
rlatofha <89819389+rlatofha@users.noreply.github.com>
2021-09-08 17:51:08 +0900

    곱셈 파일 드립니다.
    새롬이가 작업함.

---
+++ OOP_04/src/com/javalec/multi/multiplication.java
@@ -0,0 +1,24 @@
+package com.javalec.multi;
+
+public class multiplication {
+
+	//field
+	int fNum = 0;
+	int sNum = 0;
+	
+	//constructor
+	
+	public multiplication(int fNum, int sNum) {
+		super();
+		this.fNum = fNum;
+		this.sNum = sNum;
+	}
+	
+	//method
+	
+	public int multiplication(){
+		int result = fNum * sNum; //함수가 리턴해주려고 만든 값이다.
+		return result;
+	}
+
+}
